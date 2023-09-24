3302c521d238fadae14fa5e67a137ed2873e7431
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2023-09-24 20:54:05 +0530

    added pdf-merger

---
+++ py-scripting/pdf-merger.py
@@ -0,0 +1,18 @@
+# IMPORTING THE LATEST VERSION OF pyPDF2-3.0.1
+from pyPDF2 import PdfMerger
+import sys
+
+# fetching the file names in the command palette :
+inputs = sys.argv[1:]
+
+#defining the function to merge n-pdfFiles :
+def pdf_merger(pdf_list):
+    merger = PdfMerger()
+    for pdf in pdf_list:
+        merger.append(pdf)
+    merger.write('Super.pdf') #storing the merged pdfFiles in a single file...
+    merger.close()
+
+
+
+
