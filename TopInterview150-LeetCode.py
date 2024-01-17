a60acac0337c1820aa1824ae4e349ee9138c7c3f
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2024-01-17 21:35:03 +0530

    LC - ArrayRotater

--- TopInterview150-LeetCode.py
+++ TopInterview150-LeetCode.py
@@ -9,3 +9,18 @@ def mostWater(height) -> int:
         else:
             bound -= 1
     return maxArea
+
+
+# ARRAY ROTATION :
+def rightRotater(arr, k) -> list[int]:
+    def rotater(nums, start, end):
+        while start < end:
+            nums[start], nums[end] = nums[end], nums[start]
+            start += 1
+            end -= 1
+
+    k %= len(arr)
+    rotater(arr, 0, len(arr) - 1)
+    rotater(arr, k, len(arr) - 1)
+    rotater(arr, 0, k - 1)
+    return arr
