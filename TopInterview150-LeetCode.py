3d82a1ce57008d9c7e0b6507f7659361c18d0936
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2024-01-20 22:07:21 +0530

    added-spiralMatrixOrder

--- TopInterview150-LeetCode.py
+++ TopInterview150-LeetCode.py
@@ -40,3 +40,33 @@ def findMedianSortedArrays(nums1, nums2) -> float:
 # POWER (X, N) :
 def powerFunc(x: float, n: int) -> float:
     return x**n
+
+
+# CLOCKWISE - SPIRAL MATRIX :
+def spiralOrder(matrix):
+    spiralMatrix = []
+    top = 0
+    left = 0
+    right = len(matrix[0]) - 1
+    bottom = len(matrix) - 1
+
+    while top <= bottom and left <= right:
+        for i in range(left, right + 1):
+            spiralMatrix.append(matrix[top][i])
+        top += 1
+
+        for i in range(top, bottom + 1):
+            spiralMatrix.append(matrix[i][right])
+        right -= 1
+
+        if top <= bottom:
+            for i in range(right, left - 1, -1):
+                spiralMatrix.append(matrix[bottom][i])
+            bottom -= 1
+
+        if left <= right:
+            for i in range(bottom, top - 1, -1):
+                spiralMatrix.append(matrix[i][left])
+            left += 1
+
+    return spiralMatrix
