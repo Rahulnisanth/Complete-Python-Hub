06590fa6a3b60ea3fde78411b253ce91316b2e38
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2024-01-28 14:51:43 +0530

    added-plusOneArray

--- TopInterview150-LeetCode.py
+++ TopInterview150-LeetCode.py
@@ -172,3 +172,14 @@ def groupAnagrams(strs) :
             sortedWord = ''.join(sorted(word))
             myDict[sortedWord].append(word)
         return list(myDict.values())
+
+
+# ARRAY PLUS ONE :
+def plusOne(digits):
+        for i in range(len(digits) - 1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            digits[i] = 0
+        digits = [1] + [0] * len(digits)
+        return digits
