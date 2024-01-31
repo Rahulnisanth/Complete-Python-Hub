abc969789537840ea80a070eadcaafb776134876
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2024-01-31 21:45:32 +0530

    added-summaryRanges

--- TopInterview150-LeetCode.py
+++ TopInterview150-LeetCode.py
@@ -209,3 +209,25 @@ def mySqrt(x: int) -> int:
             else:
                 end = mid - 1
         return end
+
+
+# SUMMARY RANGES :
+def summaryRanges(nums):
+        if len(nums) < 1:
+            return []
+        else:
+            lister = []
+            start = nums[0]
+            for i in range(1, len(nums)):
+                if nums[i] != nums[i - 1] + 1:
+                    if start == nums[i - 1]:
+                        lister.append(str(start))
+                    else:
+                        lister.append(str(start) + "->" + str(nums[i - 1]))
+                    start = nums[i]
+            if start == nums[-1]:
+                lister.append(str(start))
+            else:
+                lister.append(str(start) + "->" + str(nums[-1]))
+    
+            return lister
