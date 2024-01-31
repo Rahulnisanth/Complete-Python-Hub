d0d07e4138c90c0a7b84c1753f72bc715235301a
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2024-01-31 22:07:10 +0530

    added-dailyTemperature

--- TopInterview150-LeetCode.py
+++ TopInterview150-LeetCode.py
@@ -231,3 +231,16 @@ def summaryRanges(nums):
                 lister.append(str(start) + "->" + str(nums[-1]))
     
             return lister
+
+
+# DAILY TEMPERATURES :
+def dailyTemperatures(temperatures) :
+        n = len(temperatures)
+        waitingDays = [0] * n
+        stack = []
+        for i in range(n):
+            while stack and temperatures[i] > temperatures[stack[-1]]:
+                prev_idx = stack.pop()
+                waitingDays[prev_idx] = i - prev_idx
+            stack.append(i)
+        return waitingDays
