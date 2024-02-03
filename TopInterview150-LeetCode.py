c577ae3e158dd99e55e0b9d1377bccd84ca898f6
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2024-02-03 20:07:04 +0530

    added-minSubArrayDP

--- TopInterview150-LeetCode.py
+++ TopInterview150-LeetCode.py
@@ -269,3 +269,30 @@ def maxPoints(points) -> int:
                     current_max = max(current_max, slopes[slope])
             max_points = max(max_points, current_max + duplicate_points + 1)
         return max_points
+
+
+# SIMPLIFIED PATH [FOLDER PATHS] :
+def simplifyPath(path: str) -> str:
+        directories = path.split('/')
+        stack = []
+        for files in directories:
+            if files == '' or files == '.':
+                continue
+            elif files == '..':
+                if stack:
+                    stack.pop()
+            else:
+                stack.append(files)
+        return '/' + '/'.join(stack)
+
+
+# MIN SUB ARRAY USING DYNAMIC PROGRAMMING :
+def maxSumAfterPartitioning(arr , k) -> int:
+        n = len(arr)
+        dp = [0] * (n + 1)
+        for i in range(1, n + 1):
+            max_val = float('-inf')
+            for j in range(1, min(i, k) + 1):
+                max_val = max(max_val, arr[i - j])
+                dp[i] = max(dp[i], dp[i - j] + max_val * j)
+        return dp[n]
