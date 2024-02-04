41e270cce4b53be9d1562187d26db8f6bea5ddb2
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2024-02-04 13:08:03 +0530

    added-pinArrayDecode

--- TopInterview150-LeetCode.py
+++ TopInterview150-LeetCode.py
@@ -296,3 +296,25 @@ def maxSumAfterPartitioning(arr , k) -> int:
                 max_val = max(max_val, arr[i - j])
                 dp[i] = max(dp[i], dp[i - j] + max_val * j)
         return dp[n]
+
+
+# DECODE PIN ARRAY :
+def cumulativeSum(n):
+    cum = 0
+    while n != 0:
+        cum += n % 10
+        n //= 10
+    return cum if cum < 10 else cumulativeSum(cum) 
+
+def decode_pin(pinArray):
+    cumSum = []
+    for num in pinArray:
+        cumSum.append(cumulativeSum(num))
+        
+    result = ""
+    for digit in cumSum:
+        if digit % 2 != 0:
+            result += chr(ord("a") + digit)
+        else:
+            result += str(digit)
+    return result
