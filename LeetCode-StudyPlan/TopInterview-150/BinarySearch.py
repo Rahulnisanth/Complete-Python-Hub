1b1c8d8252a0bbd7c677daa7afb7edf409bc850e
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2024-02-19 21:46:42 +0530

    added-searchNearest/IndexPosition

--- LeetCode-StudyPlan/TopInterview-150/BinarySearch.py
+++ LeetCode-StudyPlan/TopInterview-150/BinarySearch.py
@@ -7,3 +7,20 @@ def findMedianSortedArrays(nums1, nums2) -> float:
         return (nums1[mid] + nums1[(mid) - 1]) / 2
     else:
         return nums1[mid] 
+
+
+# SEARCH INSERT POSITION :
+def searchInsert(nums, target: int) -> int:
+    start, mid = 0, 0
+    end = len(nums) - 1
+    
+    while start <= end:
+        mid = (start + end) // 2
+        if nums[mid] == target:
+            return mid
+        elif target < nums[mid]:
+            end = mid - 1
+        else:
+            start = mid + 1
+
+    return start
