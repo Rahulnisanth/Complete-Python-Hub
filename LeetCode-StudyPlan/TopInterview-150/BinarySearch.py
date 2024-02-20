98b88921a0bf29c0dc3596e66379b14bf1263015
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2024-02-20 12:30:43 +0530

    added-findingPeakElement

--- LeetCode-StudyPlan/TopInterview-150/BinarySearch.py
+++ LeetCode-StudyPlan/TopInterview-150/BinarySearch.py
@@ -24,3 +24,18 @@ def searchInsert(nums, target: int) -> int:
             start = mid + 1
 
     return start
+
+
+# FINDING THE PEAK ELEMENT :
+def findPeakElement(nums) -> int:
+    start, end = 0, len(nums) - 1
+    mid = 0
+
+    while start < end:
+        mid = (start + end) // 2
+        if nums[mid] > nums[mid + 1]:
+            end = mid
+        else:
+            start = mid + 1
+    
+    return start
