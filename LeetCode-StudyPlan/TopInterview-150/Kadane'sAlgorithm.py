aadaaca06d6877deffbaeb6b4b4b6e7fdde2e3a9
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2024-03-02 12:06:59 +0530

    added-circularMaxxSubArray

--- LeetCode-StudyPlan/TopInterview-150/Kadane'sAlgorithm.py
+++ LeetCode-StudyPlan/TopInterview-150/Kadane'sAlgorithm.py
@@ -8,3 +8,20 @@ def maxSubArray(nums) -> int:
         max_sum = max(max_sum, current_sum)
 
     return max_sum
+
+
+# FINDING THE MAXIMUM SUB-ARRAY IN CIRCULAR ARRAY :
+def maxSubarraySumCircular(nums) -> int:
+    total, currMaxx, currMin = 0, 0, 0
+    maxx = float('-inf')
+    minn = float('inf')
+
+    for num in nums:
+        total += num
+        currMaxx = max(num, currMaxx + num)
+        currMin = min(num, currMin + num)
+        maxx = max(maxx, currMaxx)
+        minn = min(minn, currMin)
+        
+    return maxx if maxx < 0 else max(maxx, total - minn)
+    
