2a82ed1ea90a0e2325b699654716a3bf5f0d87ae
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2024-03-03 11:27:23 +0530

    added-backTrackingLetterCombinations

---
+++ LeetCode-StudyPlan/TopInterview-150/BackTracking.py
@@ -0,0 +1,26 @@
+# PHONE NUMBER + LETTER COMBINATIONS :
+def letterCombinations(digits: str):
+    if not digits:
+        return []
+    result = []
+    digits_to_letters = {
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz'
+        }
+
+    def backtrack(current, idx):
+        if len(current) == len(digits):
+            result.append(current)
+            return
+        else:
+            for letter in digits_to_letters[digits[idx]]:
+                backtrack(current + letter, idx + 1)
+    
+    backtrack('', 0)
+    return result
