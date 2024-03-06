75085966e37d82906ff779ce8da93ceb1221ac56
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2024-03-06 10:15:58 +0530

    added-combinations

--- LeetCode-StudyPlan/TopInterview-150/BackTracking.py
+++ LeetCode-StudyPlan/TopInterview-150/BackTracking.py
@@ -41,3 +41,17 @@ def permute(nums) :
     resultList = []
     backtrack(resultList, [], nums)
     return resultList
+
+
+# COMBINATIONS :
+def combine(n: int, k: int):
+    def backtrack(start, path):
+        if len(path) == k:
+            result.append(path[:])
+        for i in range(start, n + 1):
+            path.append(i)
+            backtrack(i + 1, path)
+            path.pop()
+    result = []
+    backtrack(1, [])
+    return result
