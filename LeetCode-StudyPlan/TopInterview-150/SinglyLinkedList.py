7af6ed370c85cd8e82b1689b52e417845723312e
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2024-03-10 12:38:30 +0530

    added-reverseLinkedList-II

--- LeetCode-StudyPlan/TopInterview-150/SinglyLinkedList.py
+++ LeetCode-StudyPlan/TopInterview-150/SinglyLinkedList.py
@@ -63,3 +63,23 @@ def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) ->
         curr = curr.next
     curr.next = list1 or list2
     return dummy.next
+
+
+# REVERSE THE LINKED LIST WITHIN GIVEN BOUNDARY :
+def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+    dummy = ListNode(0, head)
+
+    leftPrev, curr = dummy, head
+    for i in range(left - 1):
+        leftPrev, curr = curr, curr.next
+    
+    prev = None
+    for i in range(right - left + 1):
+        tempNext = curr.next
+        curr.next = prev
+        prev, curr = curr, tempNext
+    
+    leftPrev.next.next = curr
+    leftPrev.next = prev
+
+    return dummy.next
