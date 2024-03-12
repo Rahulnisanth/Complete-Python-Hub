0682a6dfa71dd27de358f10fec8bf915bbbfadba
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2024-03-12 20:14:09 +0530

    added-newFile

--- LeetCode-StudyPlan/TopInterview-150/SlidingWindow.py
+++ LeetCode-StudyPlan/TopInterview-150/SlidingWindow.py
@@ -29,3 +29,31 @@ def lengthOfLongestSubstring(s: str) -> int:
                 max_length = max(max_length, end - start + 1)
             char_index[s[end]] = end
         return max_length
+
+
+# SUBSTRING WITH ALL THE CONCATENATION OF THE WORD ARRAY :
+def findSubstring(s: str, words):
+    result = []
+    length = len(words[0])
+    # Map to store the frequency :
+    word_map = {}
+    for word in words:
+        word_map[word] = word_map.get(word, 0) + 1
+
+    for i in range(len(s) - length * len(words) + 1):
+        copy_map = word_map.copy() # Copy map to track the frequencies ...
+        for j in range(len(words)):
+            word = s[(i + j * length) : (i + j * length + length)]
+            if word in copy_map:
+                count = copy_map[word]
+                if count == 1:
+                    del copy_map[word]
+                else:
+                    copy_map[word] = count - 1
+                if not copy_map:
+                    result.append(i)
+                    break
+            else:
+                break
+
+    return result
--- LeetCode-StudyPlan/TopInterview-150/Strings.py
+++ LeetCode-StudyPlan/TopInterview-150/Strings.py
@@ -7,3 +7,11 @@ def mergeAlternately(word1: str, word2: str) -> str:
         result += word2[i]
     result += word1[n:] + word2[n:]
     return "".join(result)
+
+
+# GCD OF TWO STRINGS :
+from math import gcd
+def gcdOfStrings(str1: str, str2: str) -> str:
+    if str1 + str2 != str2 + str1 :
+        return ""
+    return str1[ : gcd(len(str1), len(str2))]
