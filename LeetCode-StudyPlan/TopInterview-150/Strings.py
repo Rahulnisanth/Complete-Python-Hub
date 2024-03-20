e4492ed31f88607ed88fadc188b09efce13ee31a
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2024-03-20 10:10:20 +0530

    added-vowels reversal

--- LeetCode-StudyPlan/TopInterview-150/Strings.py
+++ LeetCode-StudyPlan/TopInterview-150/Strings.py
@@ -15,3 +15,24 @@ def gcdOfStrings(str1: str, str2: str) -> str:
     if str1 + str2 != str2 + str1 :
         return ""
     return str1[ : gcd(len(str1), len(str2))]
+
+
+# VOWELS REVERSAL :
+def reverseVowels(s: str) -> str:
+    vowels = set('aeiouAEIOU')
+    s = list(s)
+    start, end = 0, len(s) - 1
+    while start < end:
+        if s[start] in vowels and s[end] in vowels:
+            s[start],s[end] = s[end],s[start]
+            start += 1
+            end -= 1
+        elif s[start] in vowels:
+            end -= 1
+        elif s[end] in vowels:
+            start += 1
+        else:
+            start += 1
+            end -= 1
+
+    return "".join(s)
