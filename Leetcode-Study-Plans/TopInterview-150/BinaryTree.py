8a4030348ef2a54c0c98622969852a9df5f1718b
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2024-04-06 19:26:24 +0530

    added-binaryFile

--- Leetcode-Study-Plans/TopInterview-150/BinaryTree.py
+++ Leetcode-Study-Plans/TopInterview-150/BinaryTree.py
@@ -4,3 +4,23 @@ def maxDepth(root) -> int:
         left, right = maxDepth(root.left), maxDepth(root.right)
         return 1 + max(left, right)
     return 0
+
+
+# IS SAME TREE :
+def isSameTree(p, q):
+    if p == None and q == None :
+        return True
+    if p != None and q != None and p.val == q.val :
+        return isSameTree(p.left, q.left) and isSameTree(p.right, q.right)
+    return False
+
+
+# INVERT THE BINARY TREE :
+def invertTree(root):
+    if not root: #Base Case
+        return root
+    invertTree(root.left) 
+    invertTree(root.right)  
+
+    root.left, root.right = root.right, root.left
+    return root 
