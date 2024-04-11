1a2d665954f441b61db6e1d44b3ece93baae7f58
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2024-04-11 20:26:30 +0530

    ./

--- Leetcode-Study-Plans/Ace-75s/Stack.py
+++ Leetcode-Study-Plans/Ace-75s/Stack.py
@@ -67,3 +67,21 @@ def countStudents(students, sandwiches) -> int:
         counts[sandwich] -= 1
         remain -= 1
     return remain
+
+
+# REMOVE K DIGITS TO MAKE THE NUMBER SMALLEST :
+def removeKdigits(num: str, k: int) -> str:
+    stack = []
+    for digit in num:
+        while k > 0 and stack and stack[-1] > digit:
+            stack.pop()
+            k -= 1
+        stack.append(digit)
+
+    while k > 0:
+        stack.pop()
+        k -= 1
+    
+    result = ''.join(stack).lstrip('0')
+
+    return result if result else '0'
