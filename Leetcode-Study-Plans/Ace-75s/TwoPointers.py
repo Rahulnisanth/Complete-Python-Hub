e5f681f5049ec9938d2398585db8b4ef162dc897
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2024-04-15 19:46:06 +0530

    .//

--- Leetcode-Study-Plans/Ace-75s/TwoPointers.py
+++ Leetcode-Study-Plans/Ace-75s/TwoPointers.py
@@ -30,3 +30,19 @@ def mostWater(height) -> int:
         else:
             bound -= 1
     return maxArea
+
+
+# MAXIMUM NUMBER OF K-PAIR SUM:
+def maxOperations(nums, k: int) -> int:
+    nums.sort()
+    count, left, right = 0, 0, len(nums) - 1
+    while left < right:
+        if nums[left] + nums[right] == k :
+            count += 1
+            left += 1
+            right -= 1
+        elif nums[left] + nums[right] < k:
+            left += 1
+        else :
+            right -= 1
+    return count
