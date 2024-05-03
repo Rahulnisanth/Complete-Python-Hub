f4cc7aa7eb8830641879c5765c882b74ac9a836a
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2024-05-03 21:58:40 +0530

    added-new File

--- Leetcode-Study-Plans/Ace-75s/Arrays&Strings.py
+++ Leetcode-Study-Plans/Ace-75s/Arrays&Strings.py
@@ -151,3 +151,27 @@ def uniqueOccurrences(arr) -> bool:
             return False
     
     return True
+
+
+# COMPARE VERSION NUMBERS :
+def compareVersion(version1: str, version2: str) -> int:
+    # Helper function to find the load on the string :
+    def helper(input, idx):
+        num = 0
+        while idx < len(input):
+            if input[idx] == '.':
+                break
+            else:
+                num = num * 10 + int(input[idx])
+            idx += 1
+        return [num, idx + 1]
+    # Main method defs :
+    i = j = 0
+    while i < len(version1) or j < len(version2):
+        v1, i = helper(version1, i)
+        v2, j = helper(version2, j)
+        if v1 > v2:
+            return 1
+        elif v1 < v2:
+            return -1
+    return 0
