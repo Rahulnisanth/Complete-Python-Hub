33e7e31587d0b8393d841962786758be642c3165
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2024-05-15 20:40:17 +0530

    added-newGridSum

--- Leetcode-Study-Plans/Ace-75s/2DArrays.py
+++ Leetcode-Study-Plans/Ace-75s/2DArrays.py
@@ -26,3 +26,46 @@ def largestArea(heights):
             maxArea = max(maxArea, height * width)
         stack.append(i)
     return maxArea
+
+
+# FIND THE SAFEST PATH IN GRID :
+from collections import deque
+import heapq
+def maximumSafenessFactor(grid) -> int:
+        n = len(grid)
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        visits = [[float('inf')] * n for _ in range(n)]
+        queue = deque()
+
+        for row in range(n):
+            for col in range(n):
+                if grid[row][col] == 1:
+                    queue.append((row, col))
+                    visits[row][col] = 0
+
+        while queue:
+            row, col = queue.popleft()
+            for dr, dc in directions:
+                nr, nc = row + dr, col + dc
+                if 0 <= nr < n and 0 <= nc < n and visits[nr][nc] == float('inf'):
+                    visits[nr][nc] = visits[row][col] + 1
+                    queue.append((nr, nc))
+
+        max_heap = [(-visits[0][0], 0, 0)]
+        max_safeness = [[-1] * n for _ in range(n)]
+        max_safeness[0][0] = visits[0][0]
+
+        while max_heap:
+            d, row, col = heapq.heappop(max_heap)
+            d = -d
+            if row == n - 1 and col == n - 1:
+                return d  
+
+            for dr, dc in directions:
+                nr, nc = row + dr, col + dc
+                if 0 <= nr < n and 0 <= nc < n:
+                    new_safe = min(d, visits[nr][nc])
+                    if new_safe > max_safeness[nr][nc]:
+                        max_safeness[nr][nc] = new_safe
+                        heapq.heappush(max_heap, (-new_safe, nr, nc))
+        return -1  
