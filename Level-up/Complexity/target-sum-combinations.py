a0b3764eea3957ed0fb366a1fcc093d95ca322aa
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2024-06-10 19:12:13 +0530

    //

--- Level-up/Complexity/target-sum-combinations.py
+++ Level-up/Complexity/target-sum-combinations.py
@@ -1,32 +1,38 @@
-def target_sum_make_combinations(nums, target):
-    result, dump = [], []
-    nums.sort()
-    def helper(idx, remain):
-        if remain == 0:
-            dump.sort()
-            if dump[:] not in result and sum(dump[:]) == target: 
-                result.append(dump[:])
-            return
-        for i in range(idx, len(nums)):
-            if i > idx and nums[i] == nums[i - 1]: continue
-            if nums[i] > remain: break
-            dump.append(nums[i])
-            helper(idx + 1, remain - nums[i])
-            dump.pop()
-    helper(0, target)
-    return result
+def target_sum_combinations(test_cases):
+    results = []
+    for case in test_cases:
+        n, array, target = case
+        array.sort()
+        combinations = []
+        def backtrack(start, path, target):
+            if target == 0:
+                combinations.append(path[:])
+                return
+            for i in range(start, n):
+                if i > start and array[i] == array[i - 1]:
+                    continue
+                if array[i] > target:
+                    break
+                path.append(array[i])
+                backtrack(i + 1, path, target - array[i])
+                path.pop()
+        backtrack(0, [], target)
+        if not combinations:
+            results.append("Empty")
+        else:
+            dumps = ''.join(['( ' + ' '.join(map(str, dump)) + ' )' for dump in combinations])
+            results.append(dumps)
+    return results
 
-# Input stream :
-test = int(input())
-for _ in range(test):
-    n = int(input())
-    nums = list(map(int, input().split()))
-    target = int(input())
-    answer = target_sum_make_combinations(nums, target)
-    if len(answer) > 0:
-        for i in range(len(answer)):
-            print("(", end='')
-            for j in range(len(answer[i])):
-                print(f" {answer[i][j]}", end='')
-            print(")", end='')
-    else: print("Empty")
+# Input Stream :
+_test = int(input().strip())
+test_cases = []
+for _ in range(_test):
+    N = int(input().strip())
+    array = list(map(int, input().strip().split()))
+    B = int(input().strip())
+    test_cases.append((N, array, B))
+
+results = target_sum_combinations(test_cases)
+for result in results:
+    print(result)
