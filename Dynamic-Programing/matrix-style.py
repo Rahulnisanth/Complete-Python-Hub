a898b049bed456546c502d4bc1b914282a3d06ba
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2024-07-15 19:20:28 +0530

    added-File-in-dp

--- Dynamic-Programing/matrix-style.py
+++ Dynamic-Programing/matrix-style.py
@@ -39,3 +39,16 @@ def uniquePathsWithObstacles(self, obstacleGrid) -> int:
             else:
                 dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
     return dp[m][n]
+
+
+# MIN PATH SUM IN TRIANGLE [BOTTOM-UP APPROACH] :
+def minimumTotal(triangle) -> int:
+    n = len(triangle)
+    dp = [[0] * (i + 1) for i in range(n)]
+    for i in range(n - 1, -1, -1):
+        for j in range(i + 1):
+            if i == n - 1:
+                dp[i][j] = triangle[i][j]
+            else:
+                dp[i][j] = triangle[i][j] + min(dp[i + 1][j], dp[i + 1][j + 1])
+    return dp[0][0]
