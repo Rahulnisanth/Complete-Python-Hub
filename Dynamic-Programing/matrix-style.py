a0935cbabf238558c61dde3d423cec0e1ad75f20
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2024-07-16 14:29:53 +0530

    //completed-matrix-dps

--- Dynamic-Programing/matrix-style.py
+++ Dynamic-Programing/matrix-style.py
@@ -70,3 +70,19 @@ def minFallingPathSum(matrix) -> int:
                 if j == m - 1:
                     matrix[i][j] += min(matrix[i - 1][j], matrix[i - 1][j - 1])
     return min(matrix[n - 1])
+
+
+# MAXIMAL SQUARE :
+def maximalSquare(matrix) -> int:
+    if not matrix or len(matrix) < 1:
+        return 0
+    else:
+        n, m = len(matrix), len(matrix[0])
+        dp = [[0] * (m + 1) for _ in range(n + 1)]
+        max_area = 0
+        for i in range(1, n + 1):
+            for j in range(1, m + 1):
+                if matrix[i - 1][j - 1] == '1':
+                    dp[i][j] = 1 + min(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1])
+                    max_area = max(max_area, dp[i][j])
+        return (max_area * max_area)
