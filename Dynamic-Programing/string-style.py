9314e9052054020dc6c014af8e1506e8b5fedf4d
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2024-07-21 14:43:18 +0530

    //

--- Dynamic-Programing/string-style.py
+++ Dynamic-Programing/string-style.py
@@ -47,3 +47,22 @@ def longestPalindromicSubSequence(S) -> int:
             else:
                 dp[i][j] = max(dp[i][j - 1], dp[i - 1][j])
     return dp[-1][-1]
+
+
+# EDIT DISTANCE >> MIN OPERATIONS TO CONVERT WORD 1 TO WORD 2 :
+def minDistance(self, word1: str, word2: str) -> int:
+    dp = [[0] * (len(word2) + 1) for _ in range(len(word1) + 1)]
+    # First row :
+    for i in range(len(word2) + 1):
+        dp[0][i] = i
+    # First column :
+    for j in range(len(word1) + 1):
+        dp[j][0] = j
+    # Edge cases:
+    for i in range(1, len(word1) + 1):
+        for j in range(1, len(word2) + 1):
+            if word1[i - 1] == word2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1]
+            else:
+                dp[i][j] = min(dp[i][j - 1], dp[i - 1][j - 1], dp[i - 1][j]) + 1
+    return dp[-1][-1]
