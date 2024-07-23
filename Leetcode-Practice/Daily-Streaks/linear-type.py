3ab0c44b269c191d6ff0e4aaabd875d9605d9294
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2024-07-23 13:23:53 +0530

    //added-daily-streak-leetcode

--- Leetcode-Practice/Daily-Streaks/linear-type.py
+++ Leetcode-Practice/Daily-Streaks/linear-type.py
@@ -5,3 +5,10 @@ def sortPeople(names, heights):
         mapper[height] = name
     mapper = sorted(mapper.items(), key=lambda x: x[0], reverse=True)
     return [mapper[i][1] for i in range(len(mapper))]
+
+
+# SORT THE ARRAY BASED ON FREQUENCIES :
+from collections import Counter
+def frequencySort(nums):
+    freq = Counter(nums)
+    return sorted(nums, key=lambda x: (freq[x], -x))
