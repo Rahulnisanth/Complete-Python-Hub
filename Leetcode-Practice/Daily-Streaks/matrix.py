6cf82c67c71bde46bfd68cc54984a15103794c22
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2024-08-08 10:10:27 +0530

    added-daily-streak

--- Leetcode-Practice/Daily-Streaks/matrix.py
+++ Leetcode-Practice/Daily-Streaks/matrix.py
@@ -14,15 +14,26 @@ def luckyNumbers (matrix):
 
 
 # BUILD MATRIX USING ROW-SUM AND COLUMN-SUM :
-def restoreMatrix(rowSum, colSum):
-    row, col = len(rowSum), len(colSum)
-    matrix = [[0] * (col) for _ in range(row)]
-    i = j = 0
-    while i < row and j < col:
-        x = min(rowSum[i], colSum[j])
-        matrix[i][j] = x
-        rowSum[i] -= x
-        colSum[j] -= x
-        i += (rowSum[i] == 0)
-        j += (colSum[j] == 0)
-    return matrix
+def spiralMatrixIII(self, rows, cols, rStart, cStart):
+    i, j = rStart, cStart
+    dir_x, dir_y = 0, 1 
+    twice = 2
+    res = []
+    moves = 1
+    next_moves = 2
+    while len(res) < (rows * cols):
+        if (-1 < i < rows) and ( -1 < j < cols):
+            res.append([i,j])
+        i += dir_x
+        j += dir_y
+        moves -= 1
+        if moves == 0:
+            dir_x, dir_y = dir_y, -dir_x 
+            twice -= 1
+            if twice == 0:
+                twice = 2
+                moves = next_moves
+                next_moves += 1
+            else:
+                moves = next_moves - 1
+    return res
