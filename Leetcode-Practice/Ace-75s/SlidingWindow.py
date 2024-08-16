d2644f2c465773ee48cf0c80adbb1aaf2514c2eb
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2024-08-16 11:35:53 +0530

    //

--- Leetcode-Practice/Ace-75s/SlidingWindow.py
+++ Leetcode-Practice/Ace-75s/SlidingWindow.py
@@ -19,3 +19,20 @@ def longestOnes(nums, k) -> int:
             left += 1
         maxLen = max(maxLen, right - left + 1)
     return maxLen
+
+
+# LONGEST LENGTH OF SUB-ARRAY AFTER DELETING ONE ELEMENT :
+def longestSubArray(nums) -> int:
+    if not nums:
+        return 0
+    left, zeroes, k = 0, 0, 1
+    result = 0
+    for right in range(len(nums)):
+        if nums[right] == 0:
+            zeroes += 1
+        while zeroes > k:
+            if nums[left] == 0:
+                zeroes -= 1
+            left += 1
+        result = max(result, right - left + 1)
+    return result - k
