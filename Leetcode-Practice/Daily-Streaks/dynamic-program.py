23f0c923809c91844cb69b691c181dce822ffd7a
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2024-08-17 10:52:03 +0530

    //added-daily-streak

--- Leetcode-Practice/Daily-Streaks/dynamic-program.py
+++ Leetcode-Practice/Daily-Streaks/dynamic-program.py
@@ -10,3 +10,25 @@ def minimumDeletions(s: str) -> int:
         else:
             dp[i] = min(dp[i - 1] + 1, countB)
     return dp[-1]
+
+
+# MAXIMUM NUMBER OF POINTS WITH COST :
+def maxPoints(points) -> int:
+    M, N = len(points), len(points[0])
+    maxPoints = [0] * N
+    rightView = [0] * N
+    for row in points:
+        # Right -> Left
+        currMax = 0
+        for i in range(N - 1, -1, -1):
+            currMax = max(currMax, maxPoints[i])
+            rightView[i] = currMax
+            currMax -= 1
+        # Left -> Right
+        currMax = 0
+        for i in range(N):
+            currMax = max(currMax, maxPoints[i])
+            maxPoints[i] = max(currMax, rightView[i]) + row[i]
+            currMax -= 1
+    
+    return max(maxPoints)
