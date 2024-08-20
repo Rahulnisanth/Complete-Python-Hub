6044cd7daaf1283eb709779681cf4cf463e3a522
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2024-08-20 19:05:56 +0530

    ADDED-ACE75S

--- Leetcode-Practice/Ace-75s/LinkedList.py
+++ Leetcode-Practice/Ace-75s/LinkedList.py
@@ -29,3 +29,40 @@ def reverseList(head):
         prev = curr
         curr = fast
     return prev
+
+
+# ODD EVEN LINKED LIST :
+def oddEvenList(head):
+    odd_head = odd = ListNode(0) # type: ignore
+    even_head = even = ListNode(0) # type: ignore
+    curr = head
+    counter = 1
+    while curr:
+        if counter % 2 == 0:
+            even.next = curr
+            even = even.next
+        else:
+            odd.next = curr
+            odd = odd.next
+        curr = curr.next
+        counter += 1
+    odd.next = even_head.next
+    even.next = None
+    return odd_head.next
+
+
+# MAXIMUM TWIN SUM OF THE LINKED LIST :
+def pairSum(head) -> int:
+    if not head:
+        return 0
+    slow = fast = head
+    stack = []
+    while fast and fast.next:
+        stack.append(slow.val)
+        slow = slow.next
+        fast = fast.next.next
+    result = float("-inf")
+    while slow:
+        result = max(result, stack.pop() + slow.val)
+        slow = slow.next
+    return result
