d20599afc00b46435d65b5b16369043bc5ef8593
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2024-08-21 20:38:57 +0530

    //

--- Leetcode-Practice/Daily-Streaks/dynamic-program.py
+++ Leetcode-Practice/Daily-Streaks/dynamic-program.py
@@ -40,3 +40,17 @@ def maxPoints(points) -> int:
             dp[i][j] =  points[i - 1][j - 1] +\
                         max([dp[i - 1][col] - abs(j - col) for col in range(1, M + 1)])
     return max(dp[-1])
+
+
+# STRANGE PRINTER :
+def strangePrinter(s: str) -> int:
+    n = len(s)
+    dp = [[0] * n for _ in range(n)]
+    for i in range(n - 1, -1, -1):
+        dp[i][i] = 1
+        for j in range(i + 1, n):
+            dp[i][j] = dp[i][j - 1] + 1
+            for k in range(i, j):
+                if s[k] == s[j]:
+                    dp[i][j] = min(dp[i][j], dp[i][k] + (dp[k + 1][j - 1] if k + 1 <= j - 1 else 0))
+    return dp[0][-1]
