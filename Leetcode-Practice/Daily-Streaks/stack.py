0aae21becd26a5b686cfca1d9bb6cc9534b481f9
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2024-10-20 17:40:05 +0530

    added: daily streak file in stack

--- Leetcode-Practice/Daily-Streaks/stack.py
+++ Leetcode-Practice/Daily-Streaks/stack.py
@@ -49,3 +49,32 @@ def maxWidthRamp(nums) -> int:
             if result == j:
                 return result
     return result
+
+
+# PARSING A BOOLEAN EXPRESSION :
+def parseBoolExpr(self, expression: str) -> bool:
+    # Helper function
+    def to_bool(char: str) -> bool:
+        return char == 't'
+    
+    stack = []
+    for char in expression:
+        if char == ')':
+            sub_expr = []
+            while stack[-1] != '(':
+                sub_expr.append(stack.pop())
+            stack.pop()  
+            operator = stack.pop()  
+            
+            if operator == '!':
+                result = not self.to_bool(sub_expr[0])
+            elif operator == '&':
+                result = all(self.to_bool(val) for val in sub_expr)
+            elif operator == '|':
+                result = any(self.to_bool(val) for val in sub_expr)
+            
+            stack.append('t' if result else 'f')
+        elif char not in (',', ' '): 
+            stack.append(char)
+    
+    return to_bool(stack.pop())
