d3adb2db6ce996c846f0acd85451d326e552016b
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2024-12-24 19:41:50 +0530

    Update graph.py

--- Leetcode-Practice/Daily-Streaks/graph.py
+++ Leetcode-Practice/Daily-Streaks/graph.py
@@ -28,3 +28,47 @@ def findTheCity(n, edges, distanceThreshold) -> int:
             min_count = min_neighbors_count
             result = city
     return result
+
+
+# FIND MAXI DIAMETER AFTER MERGING TWO GRAPHS
+class Solution:
+    def minimumDiameterAfterMerge(
+        self, edges1: List[List[int]], edges2: List[List[int]]
+    ) -> int:
+        N, M = len(edges1) + 1, len(edges2) + 1
+        adjList1 = self.buildAdjacencyList(N, edges1)
+        adjList2 = self.buildAdjacencyList(M, edges2)
+        diameter1 = self.findDiameter(N, adjList1)
+        diameter2 = self.findDiameter(M, adjList2)
+        combinedDiameter = ceil(diameter1 / 2) + ceil(diameter2 / 2) + 1
+        return max(diameter1, diameter2, combinedDiameter)
+
+    def buildAdjacencyList(self, size, edges):
+        adjList = [[] for _ in range(size)]
+        for edge in edges:
+            adjList[edge[0]].append(edge[1])
+            adjList[edge[1]].append(edge[0])
+        return adjList
+
+    def findDiameter(self, N, adjList):
+        farthestNode, _ = self.findFarthestNode(N, adjList, 0)
+        _, diameter = self.findFarthestNode(N, adjList, farthestNode)
+        return diameter
+
+    def findFarthestNode(self, N, adjList, node):
+        q = deque([node])
+        visited = [False] * N
+        visited[node] = True
+        maxDis, farthestNode = 0, node
+        while q:
+            for _ in range(len(q)):
+                curr = q.popleft()
+                farthestNode = curr
+                for adj in adjList[curr]:
+                    if not visited[adj]:
+                        visited[adj] = True
+                        q.append(adj)
+            if q:
+                maxDis += 1
+        return farthestNode, maxDis
+
