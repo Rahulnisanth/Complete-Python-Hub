5f5f34955c7b6db78f80a5977955e9fac30f1055
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2024-12-26 18:17:22 +0530

    Update backtracking.py

--- Leetcode-Practice/Daily-Streaks/backtracking.py
+++ Leetcode-Practice/Daily-Streaks/backtracking.py
@@ -94,3 +94,18 @@ def diffWaysToCompute(expression: str):
         return result
     memo = {}
     return backtrack(expression, memo)
+
+
+# TARGET SUM AFTER BUILDING EXPRESSION :
+def findTargetSumWays(self, nums: List[int], target: int) -> int:
+    memo = {}
+    def dfs(idx, curr_sum):
+        if idx == len(nums):
+            return 1 if curr_sum == target else 0
+        if (idx, curr_sum) in memo:
+            return memo[(idx, curr_sum)]
+        add = dfs(idx + 1, curr_sum + nums[idx]) 
+        sub = dfs(idx + 1, curr_sum - nums[idx])
+        memo[(idx, curr_sum)] = add + sub
+        return memo[(idx, curr_sum)]
+    return dfs(0, 0)
