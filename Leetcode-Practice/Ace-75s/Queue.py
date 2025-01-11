384ee5c8dad848f58593f38a6bfe4197b82f6c43
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2025-01-11 20:45:04 +0530

    Update Queue.py

--- Leetcode-Practice/Ace-75s/Queue.py
+++ Leetcode-Practice/Ace-75s/Queue.py
@@ -8,3 +8,22 @@ class RecentCounter:
             self.count.pop(0)
         return len(self.count)
 
+
+# Dota2Senate :
+def predictPartyVictory(self, senate: str) -> str:
+    radi, dire = deque([]), deque([])
+    N = len(senate)
+    for i in range(N):
+        if senate[i] == "R":
+            radi.append(i)
+        else:
+            dire.append(i)
+
+    while radi and dire:
+        r_idx = radi.popleft()
+        d_idx = dire.popleft()
+        if r_idx < d_idx:
+            radi.append(r_idx + N)
+        else:
+            dire.append(d_idx + N)
+    return "Radiant" if radi else "Dire"
