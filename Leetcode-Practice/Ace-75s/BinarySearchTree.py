eb8dcd5f1daca84663c30293d792e03b69b9cfc6
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2025-01-11 21:13:07 +0530

    Update BinarySearchTree.py

--- Leetcode-Practice/Ace-75s/BinarySearchTree.py
+++ Leetcode-Practice/Ace-75s/BinarySearchTree.py
@@ -8,3 +8,33 @@ def searchBST(root, val: int):
         return self.searchBST(root.left, val)
     else:
         return root
+
+
+# DELETION IN BST :
+def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+    # helper func
+    def getLeftMax(node):
+        maxx = node.val
+        while node.right:
+            maxx = node.right.val
+            node = node.right
+        return maxx
+
+    if not root:
+        return
+    elif key < root.val:
+        root.left = self.deleteNode(root.left, key)
+    elif key > root.val:
+        root.right = self.deleteNode(root.right, key)
+    else:
+        # case with no/one children:
+        if not root.left:
+            return root.right
+        if not root.right:
+            return root.left
+        # case with two children
+        # get the max in left subtree
+        else:
+            root.val = getLeftMax(root.left)
+            root.left = self.deleteNode(root.left, root.val)
+    return root
