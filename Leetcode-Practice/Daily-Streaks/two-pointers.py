ed8ce8d769f205e9a6c68fc6c5c0bcd677bf7725
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2025-05-31 18:45:18 +0530

    Update two-pointers.py

--- Leetcode-Practice/Daily-Streaks/two-pointers.py
+++ Leetcode-Practice/Daily-Streaks/two-pointers.py
@@ -13,3 +13,18 @@ def areSentencesSimilar(sentence1: str, sentence2: str) -> bool:
     while j < N1 - i and j < N2 - i and words1[N1 - j - 1] == words2[N2 - j - 1]:
         j += 1
     return (i + j) == N1 or (i + j) == N2
+
+
+# REVERSE LETTERS ONLY :
+def reverseOnlyLetters(self, s: str) -> str:
+    right = len(s) - 1
+    answer = ""
+    for i, ch in enumerate(s):
+        if ch.isalpha():
+            while not s[right].isalpha():
+                right -= 1
+            answer += s[right]
+            right -= 1
+        else:
+            answer += ch
+    return answer
